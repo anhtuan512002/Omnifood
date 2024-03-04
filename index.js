6e50679f0a521bf2b4a876cca5b8c7d83b394853
anhtuan512002 <20522114@gm.uit.edu.vn>
2024-03-04 19:24:54 +0700

    New

--- index.js
+++ index.js
@@ -1,86 +1,20 @@
-// let a=0
-// document.getElementById("plus").onclick = function(){
-//     a=a+1;
-//     document.getElementById("count").innerHTML=a;
-// }
-// document.getElementById("minus").onclick = function(){
-//     a=a-1;
-//     document.getElementById("count").innerHTML=a;
-// }
-// document.getElementById("double").onclick = function(){
-//     a=a*2;
-//     document.getElementById("count").innerHTML=a;
-// }
-// document.getElementById("random").onclick = function(){
-//     a=Math.floor(Math.random()*100);
-//     document.getElementById("count").innerHTML=a;
-// }
-// document.getElementById("checkplus").onclick = function() {
-//     if (document.getElementById("checkboxs").checked) {
-//         console.log("checked");
-//     } else {
-//         console.log("unchecked");
-//     }
-// }
-// let checkraido1=document.getElementById("radio");
-// let checkraido2=document.getElementById("radio1");
-// let checkraido3=document.getElementById("radio2");
-// document.getElementById("radiocheckplus").onclick = function() {
-//     if (checkraido1.checked ) {
-//         console.log("cả 3 cái đều đã check");}
-// }
-// let e = 3;
-// let f = 1;
-// let g = 2;
+// Tạo bảng với 8 dòng và 8 cột
+function createChessBoard(rows, cols) {
+    var chessBoard = document.getElementById("chessBoard");
 
-// function sumnumber(...numbers) {
-//     if (numbers.length === 0) {
-//         return 0;
-//     }
-//     return Number(numbers[0]) + sumnumber(...numbers.slice(1));
-// }
-
-// console.log(sumnumber(e, f, g));
-// class car{
-//     constructor(power){
-//         this._gas=25;
-//         this._power=power;
-//     }
-//     get power(){
-//         return `${this._power}hp`;
-//     }
-//     get gas(){
-//         return `${this._gas}lit`;
-//     }
-// }
-// let asd=new car(100);
-// console.log(asd.power);
-// try{
-//     let x=window.prompt("nhập số");
-//     x=Number(x);
-//     if(isNaN(x)){
-//         throw new Error("nhập sai");
-//     }
-//     if(x==""){
-//         throw new Error("nhập sai");
-//     }
-//     console.log(`${x} là số`);
-// }
-// catch(err){
-//     console.log(err);
-// }
-// finally{
-//     console.log("kết thúc");
-// }
-setTimeout(firstMessage, 3000)
-setTimeout(secondMessage, 6000)
-setTimeout(thirdMessage, 9000)
-function firstMessage(){
-    alert(`buy this course to get more 1`)
+    // Tạo hàng và cột
+    for (var i = 0; i < rows; i++) {
+        var row = document.createElement("div");
+        row.className = "boardRow";
+        for (var j = 0; j < cols; j++) {
+            var square = document.createElement("div");
+            square.className = (i + j) % 2 === 0 ? "square even" : "square odd";
+            square.textContent = (i * cols) + (j + 1);
+            row.appendChild(square);
+        }
+        chessBoard.appendChild(row);
+    }
 }
-function secondMessage(){
-    alert(`buy this course to get more 2`)
-}
-function thirdMessage(){
-    alert(`buy this course to get more 2`)
-}
+window.onload = function () {
+    createChessBoard(8, 8);
+};
